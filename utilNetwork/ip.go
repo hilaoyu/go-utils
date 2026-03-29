1c19cfe407f8602c07fe9c10a9be94f48678094a
laoyu <akun.yu@xway.cn>
2026-03-29 11:59:08 +0800

    utilNetwork add func Ipv4ToInt

--- utilNetwork/ip.go
+++ utilNetwork/ip.go
@@ -37,3 +37,30 @@ func IpInCidr(ip string, cidr string) bool {
 
 	return false
 }
+
+func IpV4ToInt(ipStr string) uint32 {
+	ip,_ := Parse(ipStr)
+	if ip == nil {
+		return 0
+	}
+	ip = ip.To4()
+	if ip == nil {
+		return 0
+	}
+
+	return uint32(ip[0])<<24 |
+		uint32(ip[1])<<16 |
+		uint32(ip[2])<<8 |
+		uint32(ip[3])
+
+}
+
+
+func IntToIPV4(n uint32) string {
+	ip := make(net.IP, 4)
+	ip[0] = byte(n >> 24)
+	ip[1] = byte(n >> 16)
+	ip[2] = byte(n >> 8)
+	ip[3] = byte(n)
+	return ip.String()
+}
